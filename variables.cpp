50c0472d7759924376f84b8460e499a6e57228ce
shekh farid <shekhfarid1974@gmail.com>
2024-03-24 00:32:50 +0600

    variables

---
+++ variables.cpp
@@ -0,0 +1,46 @@
+//Integer Variable
+/*
+#include <iostream>
+
+int main(){
+    int age = 25;
+    std::cout << "Age: " << age << std::endl;
+    return 0;
+
+}
+
+
+//Double Variable
+
+#include <iostream>
+
+int main(){
+
+    double pi = 3.14159;
+    std::cout << "value of pi: " << pi << std::endl;
+    return 0;
+}
+
+
+
+//Character Variable
+
+#include <iostream>
+int main(){
+    char grade = 'A';
+    std::cout << "your Result: " << grade << std::endl;
+    return 0;
+
+}
+*/
+
+//string Variable
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string message = "Hello World";
+    std::cout << "message: " << message << std::endl;
+    return 0;
+
+}
