7b0e7f341debae35fc7d2f2a9e4e677aa164ded7
shekh farid <shekhfarid1974@gmail.com>
2024-03-31 22:48:52 +0600

    user input

---
+++ userinput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int x,y;
+int sum;
+
+cout << "Type A Number: ";
+cin >> x;
+cout << "Type Another Number: ";
+cin >> y;
+sum = x+y;
+cout << "sum is: " << sum;
+
+return 0;
+}
