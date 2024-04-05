7057b4b47b7a85e74db1ab6d3456349729fc9698
shekh farid <shekhfarid1974@gmail.com>
2024-04-05 22:10:39 +0600

    userinputsumcli

---
+++ userinputsum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int x,y;
+int sum;
+
+cout << "Type A Number: ";
+cin >> x;
+
+cout << "Type Another Number: ";
+cin >> y;
+
+sum = x+y;
+
+cout << "Sum is: " <<sum;
+
+
+
+}
