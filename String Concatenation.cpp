1b0a0cf6918bc2471a08f0f5b0496653e7223aac
shekh farid <shekhfarid1974@gmail.com>
2024-04-25 00:33:25 +0600

    String Concatenation

---
+++ String Concatenation.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+string firstname = "shekh ";
+string lastname = "farid";
+
+string fullname = firstname + lastname;
+
+cout << fullname;
+
+
+return 0;
+}
