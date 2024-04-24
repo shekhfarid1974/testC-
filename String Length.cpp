b184725d3caf7800e6c070ba4eca8e3e35eeace8
shekh farid <shekhfarid1974@gmail.com>
2024-04-25 00:51:55 +0600

    String Length

---
+++ String Length.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+string txtstring = "abcdefghijklmnopqrstuvwxyz";
+
+cout << "The length of the string is : " << txtstring.length(); //or we can use .size() function
+
+return 0;
+}
