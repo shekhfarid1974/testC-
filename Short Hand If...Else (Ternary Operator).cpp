3ffdc77b81ec712a60fa6ed5b1443ee1e8dc670f
shekh farid <shekhfarid1974@gmail.com>
2024-04-29 23:30:12 +0600

    turnary

---
+++ Short Hand If...Else (Ternary Operator).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int time = 20;
+
+
+
+//use ternary operator
+
+string result = (time < 18)? "condition 1" : "condition 2";
+
+cout << result;
+
+
+return 0;
+}
---
+++ else if.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+int time = 25;
+
+if(time < 26)
+{
+    cout << "condition 1";
+}else if(time < 18)
+{
+    cout << "condition 2";
+}else{
+
+cout << "condition 3";
+
+}
+
+
+return 0;
+}
