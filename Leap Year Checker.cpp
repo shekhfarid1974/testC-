200e90e45cda8655ba93554dd61be3f642a2ffff
shekh farid <shekhfarid1974@gmail.com>
2024-05-08 22:27:26 +0600

    leap year checke

--- Leap Year Checker.cpp
+++ Leap Year Checker.cpp
@@ -9,7 +9,7 @@ int year;
 cout << "Enter A Year: ";
 cin >> year;
 
-
+//If any of these condition is true year is leap year if none of the condition is true mane false then the year is not leap year
 if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
     cout << "Leap Year" <<endl;
 }else{
